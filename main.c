5a5227a9b94d6c14497f1199809b94a6078a13c4
Jonathon Roberts <jon@jonrob.net>
2017-08-03 23:04:48 +0100

    Added url error checking

--- main.c
+++ main.c
@@ -1,79 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <regex.h>
-#include <unistd.h>
-#include <time.h>
 #include "ezXPath.c"
 
-#define MAXELEMENTS 3000 /* Maximum number of results to return */
-
  /*
- * Using XPath to return the best odds for an event
+ * Using XPath to return when games on http://www.allkeyshop.com/ are below a certain price
  */
 
-float setreturn(int noods, float odds[]);
+#define MAX_CHAR 300
 int scrape(char * url, float thresh);
-int footballquick();
-int findraces();
-int crawlall(char *search);
-time_t getdatetime(char *datestring);
-static int cmp (const void *p1, const void *p2);
-void list();
-void printstruct(int i);
-void extracturldata(char *website);
-
-void help(char *progname){
-	fprintf(stderr,"\ Usage: %s <file>\n",progname);
-}
 
 int main(int argc, char *argv[]){
 	if(argc != 2){
-		help(argv[0]);
-		return 1;
+		fprintf(stderr,"Usage: %s <configfile>\n",argv[0]);
+		return 0;
 	}
-
-	int MAX_CHAR = 500;
 
 	char string[MAX_CHAR];
 	FILE *fp;
-	char url[300];
+	char url[MAX_CHAR];
 	float price;
 	float thresh;
 	fp=fopen(argv[1], "r");
+
 	while(fgets(string, MAX_CHAR, fp)!=NULL) {
 		strlcpy(url,  string,strlen(string));
-		if(fgets(string, MAX_CHAR, fp)==NULL)
+		if(fgets(string, MAX_CHAR, fp)==NULL){
+			fclose(fp);
 			return 0;
+		}
 		thresh = atof(string);
 		scrape(url,thresh);
-
 	}
+
+	fclose(fp);
 	return 1;
 
 }
 int scrape(char * url,float thresh){
-	char *output[MAXELEMENTS];
+	char *output[6];
 	int size;
 	float price;
-	char *xpath = "/html/body/div/div/div/div/div//p/span/@content";
-	/*
+	char *xpath = "/html/body/div/div/div/div/div/p/span/@content";
 	regex_t regex;
-	int datecheck = regcomp(&regex, ".+ [[:digit:]][[:digit:]]:",REG_EXTENDED);
-	*/
-
-	size = ezXPathHTML(url,xpath,output);
-	if(size==2){
-		if(output[0]!=NULL){
-			price = atof(output[0]);
-			if(thresh>=price)
-				printf("%s\nis on offer for %.2f\n",url,price);
-			free(output[0]);
+	int validurl = regcomp(&regex, "^http://www.allkeyshop.com/[^[:space:]]+$",REG_EXTENDED);
+	if((validurl = regexec(&regex, url,0,NULL,0))==0){
+		size = ezXPathHTML(url,xpath,output);
+		if(size==2){
+			if(output[0]!=NULL){
+				price = atof(output[0]);
+				if(thresh>=price)
+					printf("%s\nis on offer for %.2f\n",url,price);
+					free(output[0]);
+			}
+			if(output[1]!=NULL)
+				free(output[1]);
 		}
-		if(output[1]!=NULL)
-			free(output[1]);
 	}
-	//}
+	else{
+		printf("Invalid URL\n");
+	}
 }
-
-
-
